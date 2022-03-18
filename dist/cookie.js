beb0d329a5ee1fd82becf8af26858eef44b52de9
A.D <ad@Alexandrs-MacBook-Pro.local>
2022-03-17 19:13:29 -0700

    next

--- dist/cookie.js
+++ dist/cookie.js
@@ -98,24 +98,6 @@ var ad = typeof ad === "object" ? ad : {}; ad["cookie"] =
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return ADCookie; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ADCookie", function() { return ADCookie; });
-function _construct(Parent, args, Class) { if (_isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -127,8 +109,7 @@ var ADCookie = /*#__PURE__*/function () {
   _createClass(ADCookie, null, [{
     key: "create",
     value: function create() {
-      var instance = _construct(ADCookie, [root].concat(_toConsumableArray(args)));
-
+      var instance = new ADCookie();
       return instance;
     }
   }]);
@@ -145,34 +126,6 @@ var ADCookie = /*#__PURE__*/function () {
       this.defaultAttributes = {
         path: '/'
       };
-    }
-  }, {
-    key: "get",
-    value: function get(name) {
-      if (typeof document === 'undefined' || arguments.length && !name) {
-        return;
-      } // To prevent the for loop in the first place assign an empty array
-      // in case there are no cookies at all.
-
-
-      var cookies = document.cookie ? document.cookie.split('; ') : [];
-      var jar = {};
-
-      for (var i = 0; i < cookies.length; i++) {
-        var parts = cookies[i].split('=');
-        var value = parts.slice(1).join('=');
-
-        try {
-          var found = decodeURIComponent(parts[0]);
-          jar[found] = this.converterRead_(value, found);
-
-          if (name === found) {
-            break;
-          }
-        } catch (e) {}
-      }
-
-      return name ? jar[name] : jar;
     }
   }, {
     key: "set",
@@ -216,6 +169,41 @@ var ADCookie = /*#__PURE__*/function () {
       }
 
       return document.cookie = name + '=' + this.converterWrite_(value, name) + stringifiedAttributes;
+    }
+  }, {
+    key: "get",
+    value: function get(name) {
+      if (typeof document === 'undefined' || arguments.length && !name) {
+        return;
+      } // To prevent the for loop in the first place assign an empty array
+      // in case there are no cookies at all.
+
+
+      var cookies = document.cookie ? document.cookie.split('; ') : [];
+      var jar = {};
+
+      for (var i = 0; i < cookies.length; i++) {
+        var parts = cookies[i].split('=');
+        var value = parts.slice(1).join('=');
+
+        try {
+          var found = decodeURIComponent(parts[0]);
+          jar[found] = this.converterRead_(value, found);
+
+          if (name === found) {
+            break;
+          }
+        } catch (e) {}
+      }
+
+      return name ? jar[name] : jar;
+    }
+  }, {
+    key: "remove",
+    value: function remove(name, attributes) {
+      this.set(name, '', this.assign_({}, attributes, {
+        expires: -1
+      }));
     }
   }, {
     key: "converterRead_",
